f85d792ddf381749960812e5d8d586a98b666c4f
kindload <kdge248@gmail.com>
2020-06-27 19:29:17 +0900

    first_commit

--- practice_express/app3.js
+++ practice_express/app3.js
@@ -4,9 +4,9 @@ var express = require('express')
 var app = express();
 
 app.use('/', function(req, res, next){
-    console.log('세 번째 미들웨어에서 요청을 처리함.');
+    console.log('첫 번째 미들웨어에서 요청을 처리함.');
     
-    res.send({name: '소녀시대', age:20});
+    res.redirect('http://www.naver.com');
 });
 
 http.createServer(app).listen(3000,function(){
