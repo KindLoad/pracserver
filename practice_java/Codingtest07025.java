f056f5fe27244897a6eab33f52b924c2bb008cc7
kdge <kdge248@gmail.com>
2020-07-02 11:41:37 +0900

    test6

--- practice_java/Codingtest07025.java
+++ practice_java/Codingtest07025.java
@@ -3,23 +3,19 @@ package practice_java;
 public class Codingtest07025 {
     
     class Solution {
-        public int[] solution(int n, int m) {
-            int[] answer = {};
-            int max = 0, min = 0;
+        public int solution(int num) {
+            int cnt = 0;
             
-            for(int i = 1 ; i <= (n>m ? m : n) ; i++){
-                if(n%i == 0 && m%i == 0)
-                    max = i;
-            }
-            
-            for(int i = (n>m ? n : m) ; ; i++){
-                if(i%n == 0 && i%m == 0){
-                    min = i;
-                    break;
+            while(num != 1 || cnt < 500){
+                if(num%2 == 0){
+                    num /=2;
                 }
+                else
+                    num = num*3+1;
+                cnt++;
             }
-            answer = new int[]{max, min};
-            return answer;
+    
+            return (cnt >= 500) ? -1 : cnt;
         }
     }
 }
