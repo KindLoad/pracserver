58397604f6c5c195085c642434df834616e26b8c
kdge <kdge248@gmail.com>
2020-07-02 10:40:50 +0900

    forthtest

--- practice_java/Codingtest07023.java
+++ practice_java/Codingtest07023.java
@@ -3,14 +3,33 @@ package practice_java;
 public class Codingtest07023 {
     
     class Solution {
-        public long solution(long n) {
-            long answer = 0;
-            if(Math.sqrt(n)%1 == 0){
-                long temp = (int)Math.sqrt(n) + 1;
-                answer = temp * temp;
+        public int[] solution(int[] arr) {
+            int[] answer;
+            if(arr.length == 1){
+                answer = new int[1];
+                answer[0] = -1;
             }
-            else answer = -1;
-            
+            else
+            {
+                answer = new int[arr.length-1];
+                int min = arr[0];
+                int min_index = 0;
+                int j = 0;
+                for(int i = 1 ; i < arr.length ; i++){
+                    if(arr[i] < min){
+                        min = arr[i];
+                        min_index = i;
+                    }
+                }
+                for(int i = 0 ; i < answer.length ; i++){
+                    if(arr[i] == arr[min_index]){
+                        j++;
+                        continue;
+                    }
+                    answer[i] = arr[j];
+                    j++;
+                }
+            }
             return answer;
         }
     }
